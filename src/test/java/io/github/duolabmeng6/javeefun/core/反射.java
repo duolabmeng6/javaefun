18d2ba20b8ffa96949aecabe5c8c2ce85e8668ad
Erin <1>
2021-08-08 13:41:47 +0800

    更新json

--- src/test/java/io/github/duolabmeng6/javeefun/core/反射.java
+++ src/test/java/io/github/duolabmeng6/javeefun/core/反射.java
@@ -1,5 +1,24 @@
 package io.github.duolabmeng6.javeefun.core;
 
+import cn.hutool.core.lang.Console;
+import cn.hutool.core.thread.ConcurrencyTester;
+import cn.hutool.core.thread.ThreadUtil;
+import cn.hutool.core.util.RandomUtil;
+
+import java.nio.charset.StandardCharsets;
+
 public class 反射 {
+    public static void main(String[] args) {
+        ConcurrencyTester tester = ThreadUtil.concurrencyTest(9999, () -> {
+            // 测试的逻辑内容
+            long delay = RandomUtil.randomLong(100, 1000);
+            ThreadUtil.sleep(delay);
+            Console.log("{} test finished, delay: {}", Thread.currentThread().getName(), delay);
+        });
+
+// 获取总的执行时间，单位毫秒
+        Console.log(tester.getInterval());
+
+    }
 }
 
